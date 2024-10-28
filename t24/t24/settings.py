45249fd78b43578913b1eb9730adfa1fc60f5083
Arsenii Borodulin <arseniizxc@172.31.57.225-vpn.dhcp.yndx.net>
2024-10-29 01:02:00 +0300

    fix

--- t24/t24/settings.py
+++ t24/t24/settings.py
@@ -174,6 +174,16 @@ CORS_ALLOW_HEADERS = [
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
@@ -182,7 +192,6 @@ LOGGING = {
     "root": {
         "handlers": ["console"],
         "level": logging.INFO,
+        "formatter": "verbose"
     },
 }
-
-logging.info("hello")
